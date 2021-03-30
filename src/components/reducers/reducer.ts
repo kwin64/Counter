89bdbb56f54303d47d4d2f083217e47d13ddae3d
unknown <kwin11bvb@yandex.by>
2021-03-30 19:17:09 +0300

    initial commit

---
+++ src/components/reducers/reducer.ts
@@ -0,0 +1,12 @@
+export type AddNumberCountType = {
+    type: 'ADD-NEW-VALUE'
+    startValue: number,
+    maxValue: number
+}
+
+const reducer = (state: any, action: any): any => {
+    switch (action.type) {
+        case 'ADD-NEW-VALUE':
+
+    }
+}
